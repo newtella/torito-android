0243709ce477829e930b70fcd29c71a026d87285
newtella <fl.mlopezgt@gmail.com>
2020-03-20 15:47:37 -0600

    Google Maps Added, Handling no internet

--- app/src/main/java/com/example/torito/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/torito/ui/auth/LoginActivity.kt
@@ -3,17 +3,22 @@ package com.example.torito.ui.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.torito.MapsActivity
 import com.example.torito.OnBoardingActivity
 import com.example.torito.R
+import com.example.torito.data.db.AppDatabase
+import com.example.torito.data.db.entities.User
+import com.example.torito.data.network.NetworkConnectionInterceptor
+import com.example.torito.data.network.ToritoApi
+import com.example.torito.data.repositories.UserRepository
 import com.example.torito.databinding.ActivityLoginBinding
 import com.example.torito.util.hide
 import com.example.torito.util.show
+import com.example.torito.util.snackbar
 import com.example.torito.util.toast
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -23,6 +28,12 @@ class LoginActivity : AppCompatActivity(), AuthListener {
     //App Activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val networkConnectionInterceptor = NetworkConnectionInterceptor(this)
+        val api = ToritoApi(networkConnectionInterceptor)
+        val db = AppDatabase(this)
+        val repository = UserRepository(api, db)
+        val factory = AuthViewModelFactory(repository)
 
         val preferences =
             getSharedPreferences("my_preferences", MODE_PRIVATE)
@@ -38,19 +49,25 @@ class LoginActivity : AppCompatActivity(), AuthListener {
         val binding : ActivityLoginBinding = DataBindingUtil.setContentView(
             this, R.layout.activity_login)
 
-        val viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
+        val viewModel = ViewModelProvider(this, factory).get(AuthViewModel::class.java)
         binding.viewmodel = viewModel
 
         viewModel.authListener = this
 
-        //setContentView(R.layout.activity_login)
+        viewModel.getLoggedInUser().observe(this, Observer { user ->
+             if (user != null){
+                 Intent(this, MapsActivity::class.java).also {
+                     it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                     startActivity(it)
+                 }
+            }
+        })
 
         tvGoToRegister.setOnClickListener {
             Toast.makeText(this, getString(R.string.please_fill_your_register), Toast.LENGTH_SHORT).show()
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
-
     }
 
     //AuthListener Interface
@@ -60,15 +77,14 @@ class LoginActivity : AppCompatActivity(), AuthListener {
         progress_bar.show()
     }
 
-    override fun onSuccess(loginResponse: LiveData<String>) {
-        loginResponse.observe(this, Observer {
+    override fun onSuccess(user: User) {
             progress_bar.hide()
-            toast(it)
-        })
+        toast("${user.name} ha iniciado sesion")
+        //root_layout.snackbar("${user.name} is logged in")
     }
 
     override fun onFailure(message: String) {
         progress_bar.hide()
-        toast(message)
+        root_layout.snackbar(message)
     }
 }
